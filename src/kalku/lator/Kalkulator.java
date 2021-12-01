f4891576692eb35f3b2261f4ab7dd1fe7f9b62e2
ardhiesta <ardhi.mail@gmail.com>
2021-12-01 07:35:20 +0700

    first commit

---
+++ src/kalku/lator/Kalkulator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalku.lator;
+
+/**
+ *
+ * @author linuxluv
+ */
+public class Kalkulator {
+    double tambahkanBilangan(double bil1, double bil2){
+        double hasil = 0.0;
+        hasil = bil1 + bil2;
+        return hasil;
+    }
+    
+    double kurangiBilangan(double bil1, double bil2){
+        double hasil = 0.0;
+        hasil = bil1 - bil2;
+        return hasil;
+    }
+}
---
+++ src/kalku/lator/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalku.lator;
+
+/**
+ *
+ * @author linuxluv
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // object dari class MainFrame
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }
+    
+}
